8bf05ad40499edcd68afff7907657367cdb108d1
Dlean <DleanJeans@gmail.com>
2020-02-29 00:15:35 +0700

    Fix env.py for Heroku

--- env.py
+++ env.py
@@ -1,18 +1,3 @@
-from dotenv import dotenv_values
+import os
 
-def try_convert_bool(value):
-    if type(value) == str:
-        if value.lower() == 'on':
-            value = True
-        elif value.lower() == 'off':
-            value = False
-    return value
-
-def try_convert_const(value):
-    value = value.strip()
-    value = try_convert_bool(value)
-    return value
-
-for key, value in dotenv_values().items():
-    value = try_convert_const(value)
-    globals()[key] = value
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
