eaea9401a0c053eab42d1b4bc1cdf59eafff45df
Dlean <DleanJeans@gmail.com>
2020-03-19 22:07:29 +0700

    Add React cog to bot

--- bot.py
+++ bot.py
@@ -8,13 +8,19 @@ from discord.ext import commands
 prefixes = ['j ', 'jenna ', 'jen ']
 for p in prefixes[::]:
     prefixes.append(p.capitalize())
-bot = commands.Bot(command_prefix=prefixes)
+bot = commands.Bot(command_prefix=prefixes, case_insensitive=True)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game('j help'))
+    print('Logged in as', bot.user)
 
 extensions = [
     'dank_helper',
     'help',
     'alpha',
-    
+    'react',
+
     'texts',
     'images',
     's',
@@ -25,17 +31,9 @@ extensions = [
     'misc',
 ]
 
-bot.remove_command('help')
 extensions = ['cogs.' + ext for ext in extensions]
 
 if __name__ == '__main__':
     for ext in extensions:
         bot.load_extension(ext)
-
-@bot.event
-async def on_ready():
-    await bot.change_presence(activity=discord.Game('j help'))
-    print('Logged in as', bot.user)
-
-if __name__ == '__main__':
     bot.run(env.BOT_TOKEN)
