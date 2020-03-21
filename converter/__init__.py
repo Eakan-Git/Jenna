dee0c0c488ae664d56ba86c27e1c52acbe3ed77f
Dlean <DleanJeans@gmail.com>
2020-03-21 10:23:20 +0700

    converter: Fix Member type

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,3 +1,8 @@
+import discord
+import typing
+
 from converter.emojis import NitroEmojiConverter as NitroEmoji
 from converter.emojis import convert as emoji
-from converter.members import MatchingMemberConverter as Member
+from converter.members import MatchingMemberConverter
+
+Member = typing.Union[discord.Member, MatchingMemberConverter]
