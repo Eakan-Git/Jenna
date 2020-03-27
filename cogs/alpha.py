42afd57feed27aefee3bff3cb40e51d445328f7d
Dlean <DleanJeans@gmail.com>
2020-03-27 21:38:38 +0700

    Alpha: Optimize debug commands

--- cogs/alpha.py
+++ cogs/alpha.py
@@ -1,4 +1,5 @@
 import discord
+import typing
 import re
 import const
 
@@ -8,29 +9,50 @@ class Alpha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['rl'], hidden=True)
+    @commands.command(aliases=['rl'])
     @commands.is_owner()
-    async def reload(self, context, cog_name):
+    async def reload(self, context, *cogs):
         await context.trigger_typing()
-        cog = 'cogs.' + cog_name
-        self.bot.reload_extension(cog)
-        await context.send(f'Cog `{cog_name}` reloaded!')
+        responses = []
+        for cog_name in cogs:
+            cog_path = 'cogs.' + cog_name
+            response = 'Cog `%s`' % cog_name
+
+            try:
+                self.bot.reload_extension(cog_path)
+                response = f'✅ {response} reloaded!'
+            except:
+                import traceback; traceback.print_exc()
+                response = f'⚠️ {response} reload failed!'
+            
+            responses += [response]
+
+        content = '\n'.join(responses)
+        await context.send(content)
     
-    @commands.command(hidden=True)
+    @commands.command()
+    async def clearto(self, context, to_id:int):
+        await context.trigger_typing()
+        msgs_to_clear = []
+        async for msg in context.history(limit=None):
+            msgs_to_clear += [msg]
+            if msg.id == to_id:
+                break
+        await context.channel.delete_messages(msgs_to_clear)
+        await context.send(f'Deleted `{len(msgs_to_clear)}` messages!', delete_after=3)
+
+    @commands.command(aliases=['rp'])
     @commands.is_owner()
     async def repeat(self, context, msg_id:int, channel:discord.TextChannel=None):
-        if not channel:
-            channel = context.channel
+        channel = channel or context.channel
         
         async with context.typing():
-            async for msg in channel.history(limit=None):
-                if msg.id == msg_id:
-                    embed = msg.embeds[0] if msg.embeds else None
-                    await context.send(msg.content, embed=embed)
-                    return
+            msg = await channel.fetch_message(msg_id)
+            embed = msg.embeds[0] if msg.embeds else None
+            await context.send(msg.content, embed=embed)
     
-    @commands.command(hidden=True)
-    async def clean(self, context, limit:int=1):
+    @commands.command()
+    async def clean(self, context, limit:typing.Optional[int]=1, *, content=''):
         is_dm = type(context.channel) is discord.DMChannel
         is_owner = await self.bot.is_owner(context.author)
         allowed = is_owner or is_dm
@@ -42,12 +64,13 @@ class Alpha(commands.Cog):
         
         msgs_to_delete = []
         deleted = 0
-        async for m in context.history(limit=None):
-            if is_me(m) and m.id != progress_msg.id:
-                msgs_to_delete += [m]
-                deleted += 1
-                if deleted >= limit:
-                    break
+        async with context.typing():
+            async for m in context.history(limit=None):
+                if is_me(m) and m.id != progress_msg.id and content in m.content:
+                    msgs_to_delete += [m]
+                    deleted += 1
+                    if deleted >= limit:
+                        break
         
         if is_dm:
             for i, m in enumerate(msgs_to_delete):
@@ -58,7 +81,6 @@ class Alpha(commands.Cog):
             await progress_msg.edit(content=f'Deleted `{len(msgs_to_delete)}` messages!')
         
         await progress_msg.edit(delete_after=3)
-
 
 def setup(bot):
     bot.add_cog(Alpha(bot))
