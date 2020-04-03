e857cc73d0067f494a8916ec0e15ab9fd1a165f0
Dlean <DleanJeans@gmail.com>
2020-04-03 23:04:57 +0700

    Texts: Add translate command

--- cogs/texts.py
+++ cogs/texts.py
@@ -1,8 +1,11 @@
-import discord
-import upsidedown
-
 from discord.ext import commands
 from .core.dank.unscramble import unscramble
+
+import typing
+import discord
+import upsidedown
+import colors
+import googletrans
 
 MOCK_REPLACES = {
     'c': 'k',
@@ -10,14 +13,21 @@ MOCK_REPLACES = {
 }
 
 def spongebob_mock(s):
-    s = s.lower()
+    s = s.lower().replace('-', '')
     for char, sub in MOCK_REPLACES.items():
         s = s.lower().replace(char, sub)
     return ''.join(c.upper() if i % 2 else c for i, c in enumerate(s))
 
+def LangCode(s):
+    s = s.lower().replace('-', '')
+    if s in googletrans.LANGUAGES or s in googletrans.LANGCODES:
+        return s
+    raise BadArgument(f'%s is not a language code')
+
 class Texts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.translator = googletrans.Translator()
     
     @commands.command(aliases=['usd'])
     async def upsidedown(self, context, *, text):
@@ -43,6 +53,21 @@ class Texts(commands.Cog):
         else:
             response += 'Not found!'
         await context.send(response)
+    
+    @commands.command(aliases=['tr', 'tl'])
+    async def translate(self, context, dest:typing.Optional[LangCode]='en', *, text):
+        await context.trigger_typing()
+        translated = self.translator.translate(text, dest)
+        embed = colors.embed()
+        embed.description = f'{translated.text}'
+        embed.set_footer(text=f'{translated.src}: {text}')
+        await context.send(embed=embed)
+    
+    @commands.command()
+    async def listlang(self, context):
+        output = 'Supported Languages:\n'
+        output += ' '.join([f'`{code}` - {lang.title()}' for code, lang in googletrans.LANGUAGES.items()])
+        await context.send(output)
 
 def setup(bot):
     bot.add_cog(Texts(bot))
