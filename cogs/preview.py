63f229d3414e04079227a0deabf882e1fab40c53
Dlean <DleanJeans@gmail.com>
2020-05-27 07:18:08 +0700

    Preview: Add channel + jump url + embed

--- cogs/preview.py
+++ cogs/preview.py
@@ -13,23 +13,25 @@ class Preview(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.guild or message.author == self.bot.user: return
+        context = await self.bot.get_context(message)
+        if not message.guild or message.author == self.bot.user or context.command: return
 
         urls = re.findall(JUMP_URL_PATTERN, message.content)
-        context = await self.bot.get_context(message)
         for jump_url in urls:
-            jump_url = jump_url.replace('app', '')
             quoted_message = await self.message_converter.convert(context, jump_url)
             author = quoted_message.author
 
+            author_text = f'{author.display_name} #{quoted_message.channel}'
             embed = colors.embed(description=quoted_message.content) \
-                    .set_author(name=author.display_name, icon_url=author.avatar_url)
+                    .set_author(name=author_text, url=jump_url, icon_url=author.avatar_url)
             
             embed.timestamp = quoted_message.created_at
             for a in quoted_message.attachments:
                 embed.set_image(url=a.url)
             
             await context.send(embed=embed)
+            if quoted_message.embeds:
+                await context.send(embed=quoted_message.embeds[0])
 
 def setup(bot):
     bot.add_cog(Preview(bot))
